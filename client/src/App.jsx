77c6fbb827142c512360e22cdaea134cf0dd92c9
btsmith1212 <135077506+btsmith1212@users.noreply.github.com>
2023-11-12 19:23:14 -0600

    refactored app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,3 @@
-// import './App.css';
-// import { Outlet } from 'react-router-dom';
-
-// import Navbar from './components/Navbar';
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Outlet />
-//     </>
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
