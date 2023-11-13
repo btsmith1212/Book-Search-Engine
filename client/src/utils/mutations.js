7392689ddb16c2c4e515ef521377bd3d8180d6a8
btsmith1212 <135077506+btsmith1212@users.noreply.github.com>
2023-11-12 19:58:38 -0600

    refactored savedBooks

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,9 +44,9 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
-    removeBook(bookId: $bookId) {
+export const DELETE_BOOK = gql`
+  mutation deleteBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
       _id
       username
       email
