0e445c3f6f1ae6fe791a197e3229f40afd7d76c8
btsmith1212 <135077506+btsmith1212@users.noreply.github.com>
2023-11-18 21:44:32 -0600

    edited connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+const connection =
+  "mongodb+srv://btsmith1212:password1234@cluster0.pxocolz.mongodb.net/book-search-engine?retryWrites=true&w=majority";
+mongoose
+  .connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
